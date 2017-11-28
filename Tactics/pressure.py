828aae988bd621ae09dd3c11fcf37db5e4deada0
micantre <micantre@calpoly.edu>
2017-11-28 13:39:29 -0800

    fine tuned the pressure

--- Tactics/pressure.py
+++ Tactics/pressure.py
@@ -6,6 +6,7 @@ from melee.enums import Action, Button, Character
 from Chains.smashattack import SMASH_DIRECTION
 from Chains.shffl import Shffl
 from Chains.smashattack import SmashAttack
+from Chains.dashdance import DashDance
 
 class Pressure():
     chain = None
@@ -25,11 +26,26 @@ class Pressure():
             return False
 
         # We must be in close range
-        inrange = globals.gamestate.distance < 30
+        inrange = globals.gamestate.distance < 25
 
         return not sheilding and inrange
 
     def step(self):
-        self.pickchain(SmashAttack, [0, SMASH_DIRECTION.UP])
+        smashbot_state = globals.smashbot_state
+        opponent_state = globals.opponent_state
+        smashbot_position = smashbot_state.x
+        opponent_position = opponent_state.x
+        facing = smashbot_state.facing == (smashbot_position < opponent_position)
+        # Remember that if we're turning, the attack will come out the opposite way
+        if smashbot_state.action == Action.TURNING:
+            facing = not facing
+        if facing:
+            # Do the upsmash
+            self.pickchain(SmashAttack, [0, SMASH_DIRECTION.UP])
+            return
+        else:
+            # Kill the existing chain and start a new one
+            self.chain = None
+            self.pickchain(DashDance, [opponent_position])
         return
 
